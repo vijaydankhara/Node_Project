369d039fc36ea22be8f12467f0cb78979faef6e2
vijay dankhara <vijaydankhara111@gmail.com>
2024-03-27 10:04:50 +0530

    27-03-2024

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
+/*---------------------------- Admin Route --------------------------------------*/
 const adminsRoutes = require('./src/routes/admin/index.routes');
 app.use('/api/admin', adminsRoutes);
+
+/*---------------------------- Users Route --------------------------------------*/
+const usersRoutes = require('./src/routes/user/index.routes');
+app.use('/api/user', usersRoutes);
+
 
 app.listen(port, async () => {
     mongoose.connect(process.env.MONGO_DB_URL)
--- src/routes/user/index.routes.js
+++ src/routes/user/index.routes.js
@@ -1,15 +1,15 @@
 const usersRoute = require('express').Router();
 const userRoute = require('../user/user.routes');
-const productRoute = require('../user/product.routes');
-const cartRoutes = require('./cart.routes');
-const orderRoutes = require('../user/order.routes');
-const favoriteRoutes = require('../user/favorite.routes');
+// const productRoute = require('../user/product.routes');
+// const cartRoutes = require('./cart.routes');
+// const orderRoutes = require('../user/order.routes');
+// const favoriteRoutes = require('../user/favorite.routes');
 
 
 usersRoute.use('/users', userRoute);
-usersRoute.use('/product', productRoute);
-usersRoute.use('/cart', cartRoutes);
-usersRoute.use('/order', orderRoutes);
-usersRoute.use('/favorite', orderRoutes);
+// usersRoute.use('/product', productRoute);
+// usersRoute.use('/cart', cartRoutes);
+// usersRoute.use('/order', orderRoutes);
+// usersRoute.use('/favorite', orderRoutes);
 
 module.exports = usersRoute;
