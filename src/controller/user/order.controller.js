f3932e76ebdd8a5c4c9d3e0fef6403061a7f368c
vijay dankhara <vijaydankhara111@gmail.com>
2024-03-29 08:10:08 +0530

    29-03-2024

--- src/controller/user/order.controller.js
+++ src/controller/user/order.controller.js
@@ -7,21 +7,21 @@ const cartService = new CartServices();
 // Add New Order
 exports.addNewOrder = async(req, res) => {
     try {
-        let cartItems = await cartService.getCart({ user: req.user._id, isDelete: false});
+        let cartItems = await cartService.getAllCarts({ user: req.user._id, isDelete: false});
         if (!cartItems) {
-            res.status(404).json({ message: `Cart Is Not Found.....Plase Try Again......`});
+            res.status(404).json({ message: `Cart Not Found.......`});
         }
-        console.log(cartItems);
+        // console.log(cartItems);
         let orderItems = await cartItems.map(item => ({
             product: item.cartItem._id,
             quantity: item.quantity,
             price: item.cartItem.price
         }));
-        console.log(orderItems);
+        // console.log(orderItems);
         let totalPrice = orderItems.reduce((total, item) => total + (item.price * item.quantity), 100);
-        console.log(totalPrice);
+        // console.log(totalPrice);
         let newOrder = await orderServiece.addToOrder({
-            user: res.user._id,
+            user: req.user._id,
             items: orderItems,
             totalAmount: totalPrice
         });
@@ -57,7 +57,7 @@ exports.getOrder = async (req, res) => {
         let order = await orderServiece.getOrderById({_id: req.query.orderId, isDelete: false});
         console.log(order);
         if (!order) {
-            res.status(404).json({ message: `Orders Not Found..Plase Try Again...`});
+            res.status(404).json({ message: `Orders Not Found....`});
         }
         res.status(200).json(order);
     } catch (error) {
@@ -73,7 +73,7 @@ exports.deleteOrder = async (req, res) => {
         let order = await orderServiece.getOrder({_id: req.query.orderId});
         console.log(order);
         if (!order) {
-            res.status(404).json({ message: `Orders Not Found......Plase Try Again......`});
+            res.status(404).json({ message: `Orders Not Found.....`});
         }
         order = await orderServiece.updateOrder(req.body.orderId, {isDelete: true })
         res.status(200).json({order, message: `Your Order Deleted Successfully...`});
